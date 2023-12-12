71f2ecdc8e167160a3f6cd8418a5387f3305ea68
Chirila Tudor <tchirila2712@gmail.com>
2023-12-12 15:31:56 +0300

    Added Crud operations to Appointment and User

--- src/main/java/ro/chirila/programarispital/service/UserService.java
+++ src/main/java/ro/chirila/programarispital/service/UserService.java
@@ -1,7 +1,11 @@
 package ro.chirila.programarispital.service;
 
 import ro.chirila.programarispital.repository.dto.UserResponseDTO;
+import ro.chirila.programarispital.repository.dto.UserSecurityDTO;
 
 public interface UserService {
     UserResponseDTO addUser(String username);
+    UserResponseDTO getUserByUsername(String username);
+    Boolean deleteUser(String username);
+    UserSecurityDTO login(String username, String password);
 }
